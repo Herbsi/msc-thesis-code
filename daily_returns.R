0f37ada4c8619c4dbf9fd57accd6a266a8412a47
Herbsi <git@hoehenberger.me>
2024-05-22 12:04:11 +0200

    Plot the results

--- daily_returns.R
+++ daily_returns.R
@@ -3,7 +3,12 @@ set.seed(42)
 
 ## Packages
 library(dplyr)
+library(ggplot2)
+library(stringr)
 library(tibble)
+
+library(tikzDevice)
+options(tikzDefaultEngine="luatex")
 
 source("lib.R")
 ## Methods
@@ -23,7 +28,7 @@ for (i in 23:nrow(data)) {
   data[i, "X"] <- sqrt(sum(data[(i-22):(i-1), "Y"]^2))
 }
 
-data <- data[23:nrow(data), c("X", "Y")]
+data <- data[23:nrow(data), c("X", "Y")] |> mutate(test_flag = FALSE)
 
 ### Analysis -------------------------------------------------------------------
 
@@ -33,9 +38,9 @@ n.ho <- floor(nrow(data)*0.10)
 
 ## TODO <2024-05-21 Tue> `cqr' is missing.
 ## NOTE <2024-05-22 Wed> `1:6' is hard-coded.
-results <- tibble(name = c("dcp.qr", "dcp.opt", "dcp.dr", "cp.ols", "cp.loc", "dcp.idr"),
+analysis_tibble <- tibble(name = c("dcp-qr", "dcp-opt", "dcp-dr", "cp-ols", "cp-loc", "dcp-idr"),
   fn = list(dcp.qr, dcp.opt, dcp.dr, cp.ols, cp.loc, dcp.idr),
-  result = map(1:6, ~ tibble(coverage = NULL, leng = NULL))
+  results = map(1:6, ~ tibble(coverage = NULL, leng = NULL))
 )
 
 ## We do five runs.
@@ -55,14 +60,53 @@ for (r in 1:5) {
   data.train <- train_valid_split(data[ind.cp, ])[["data.train"]]
   data.valid <- train_valid_split(data[ind.cp, ])[["data.valid"]]
   data.test <- data[ind.test, ]
+  data[ind.test, "test_flag"] <- TRUE
 
-  results <- results |>
-    mutate(result = {
-      ## NOTE <2024-05-21 Tue> `fn' is a one-element list with a function for some reason; extract it with `[[1]]'
-      map(result, ~ bind_rows(.x, fn[[1]](data.train, data.valid, data.test, alpha))
+  analysis_tibble <- analysis_tibble |>
+    mutate(results = {
+      map2(fn, results, ~ bind_rows(.y, .x(data.train, data.valid, data.test, alpha))
       )
     })
 }
 
 ### Plotting -------------------------------------------------------------------
+
+## Bin analysis-data into quantiles based on test data
+X.test <- data |> filter(test_flag == TRUE) |> pull(X)
+## TODO <2024-05-22 Wed> Currently, the final bin is odd.
+analysis_tibble <- analysis_tibble |>
+  mutate(results.binned = map(results, ~ bin(.x, X.test, 20) |> filter(bin < 19))) |>
+  mutate(averages = map(results.binned, ~ summarise(.x, coverage = mean(coverage), leng = mean(leng))), .keep = "unused")
+
+plot_tibble <- analysis_tibble[, c("name", "averages")] |>
+  mutate(name = str_c("\\textsc{", name, "}")) |>
+  unnest(cols = c("name", "averages"))
+
+## Plot coverage
+## BEGIN COPY-PASTE
+tikz(file = "../tex/images/tikz/coverage.tex",
+  width = 418.2555 / 72.27) # NOTE <2024-05-22 Wed> Hard-coded `\the\textwidth' from LaTeX document here.
+print(
+ggplot(plot_tibble, aes(x = bin, y = coverage, color = as.factor(name), group = name)) +
+  geom_point(aes(shape = as.factor(name)), size = 3) +
+  geom_line() +
+  xlab("Bin") +
+  ylab("Coverage") +
+  labs(color = "Method", shape = "Method") +
+  theme_minimal()
+)
+dev.off()
+## END COPY-PASTE
+
+tikz(file = "../tex/images/tikz/length.tex",
+    width = 418.2555 / 72.27) # NOTE <2024-05-22 Wed> Hard-coded `\the\textwidth' from LaTeX document here.
+ggplot(plot_tibble, aes(x = bin, y = leng, color = as.factor(name), group = name)) +
+  geom_point(aes(shape = as.factor(name)), size = 3) +
+  geom_line() +
+  xlab("Bin") +
+  ylab("Length") +
+  labs(color = "Method", shape = "Method") +
+  theme_minimal()
+dev.off()
+
 
