c3a402972bf8320ee85f55c1871aea6b2e157793
Herbsi <git@hoehenberger.me>
2024-06-14 14:28:46 +0200

    Update theorem-3.R

--- theorem-3.R
+++ theorem-3.R
@@ -1,9 +1,10 @@
 source("lib.R")
 source("plots.R")
 
-results_tibble <- theorem_3(runs = 10, n = 2^(5:10), alpha_sig = 0.1)
-pred_tibble <- pred_conditional(results_tibble)
+results_tibble <- theorem_3(runs = 10, n = 2^(5:7), alpha_sig = 0.1)
 
 plot_unconditional(results_tibble)
-
-
+plot_conditional_coverage(results_tibble)
+plot_conditional_leng(results_tibble)
+plot_conditional_mse(results_tibble)
+plot_conditional_sd(results_tibble)
