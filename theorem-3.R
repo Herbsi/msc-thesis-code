5f789684e306d2b8bf1990a23df493552ef3eac8
Herbsi <git@hoehenberger.me>
2024-08-09 08:13:41 +0200

    Revert double runs – I ran out of memory

--- theorem-3.R
+++ theorem-3.R
@@ -4,7 +4,7 @@ run_experiment(
   model = c("NI", "P", "S", "AR(P)", "AR(NI)","AR(S)"),
   method = c("CP_OLS", "CP_LOC", "DR", "IDR", "QR"),
   n = 2^c(7:14),
-  runs = 1024,
+  runs = 512,
   alpha_sig = 0.1,
   sub_dir = "theorem-3"
 )
