797a170b4bcf2998119879a10f010d135e246a5c
Herbsi <git@hoehenberger.me>
2024-08-09 08:53:09 +0200

    Revert Double runs in theorem 4

--- theorem-4.R
+++ theorem-4.R
@@ -4,6 +4,6 @@ run_experiment(
   model = c("S1(Uniform)", "S1(Beta)", "S1(Bound above)", "S1(No bounds)"),
   method = c("CP_OLS", "CP_LOC", "IDR", "IDR*", "QR", "QR*"),
   n = 2^c(7:14),
-  runs = 1024,
+  runs = 512,
   alpha_sig = 0.1,
   sub_dir = "theorem-4")
